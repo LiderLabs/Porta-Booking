cc22e98270c45d74fa425ebfad5cf6eb7992506d
Jordan Owusu-Ansah <jordanowusu03@gmail.com>
2026-06-03 11:09:26 +0000

    SPA routing vercel.json and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
+﻿import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-
 export default defineConfig({
   plugins: [react()],
+  appType: 'spa',
   resolve: {
     alias: {
       '../../convex/api': path.resolve(__dirname, 'src/convex/api.js'),
       '../../convex/dataModel': path.resolve(__dirname, 'src/convex/dataModel.d.ts'),
     }
+  },
+  server: {
+    port: 5174,
   }
 });
